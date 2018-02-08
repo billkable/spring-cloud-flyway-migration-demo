e9c1a14564d9183c911a5884d8f11e077ec7c9e2
Bill Kable <bkable@pivotal.io>
2018-02-08 14:55:18 -0600

    updated manifest and added actuator for potential of doing monitoring and migration in single app

--- src/main/java/io/pivotal/pal/flyway/flywaymigration/FlywayMigrationApplication.java
+++ src/main/java/io/pivotal/pal/flyway/flywaymigration/FlywayMigrationApplication.java
@@ -2,16 +2,22 @@ package io.pivotal.pal.flyway.flywaymigration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 
 @SpringBootApplication
-@EnableTask
+//@EnableTask
+// TODO Split Application Classes and add conditional on Task
 public class FlywayMigrationApplication {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Value("migrate-only")
+	private String migrateOnly;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlywayMigrationApplication.class, args);
@@ -24,7 +30,9 @@ public class FlywayMigrationApplication {
 			@Override
 			public void run(String... args) throws Exception {
 				// TODO Failure not handled, need to explicitly handle JDBC Migration Task here
-				logger.info("Completed flyway migration task");
+
+				if (Boolean.getBoolean(migrateOnly))
+					logger.info("Completed flyway migration task");
 			}
 		};
 	}
